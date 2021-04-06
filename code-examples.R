9d38f7715a4f33f133396e7ec8d305f4a671571e
Harrison Dekker <hdekker@uri.edu>
2021-04-06 16:27:14 -0400

    Clean up comments

--- code-examples.R
+++ code-examples.R
@@ -2,8 +2,13 @@
 # Instructor: harrison Dekker, URI Libraries and AI Lab
 # Contact: hdekker@uri.edu
 
-# Attribution: https://rpubs.com/NickCHK/RTeach2020
+# Attribution: This lesson was adapted from https://rpubs.com/NickCHK/RTeach2020
 
+################################################################################
+## SETUP
+################################################################################
+
+install.packages(c("tidyverse", "stargazer", "jtools", "swirl"))
 
 ################################################################################
 ## DATA OBJECT TYPES
@@ -13,6 +18,7 @@
 mynumber <- 1
 mycharacter <- 'one'
 myfactor <- factor(1, labels = 'one')
+
 # R variable names can contain a period and you'll see this used a lot in code 
 # samples found in textbooks, etc.
 my.logical <- TRUE
@@ -20,12 +26,9 @@ my.logical <- TRUE
 # Show the current contents of a variable
 mynumber
 
-## Questions
-mynumber + 100
-mynumber + mycharacter
-as.character(mynumber) + mycharacter
-"a" + "b"
-myfactor
+## Challenges
+# 1. How would you change the value of mynumer to 100?
+# 2. What happens if you add mynumber and mycharacter
 
 ################################################################################
 ## VECTORS
@@ -48,8 +51,9 @@ vec <- 11:20
 vec[c(1, 5, 9)]
 vec[rep(c(TRUE,FALSE),5)]
 
-# Questions
-# 1. Create a variable for a vector containing the series 100 to 200
+# Challenges
+# 1. Write the code to assign a vector containing the series 100 through 200 to
+#    a variable called myseries.
 # 2. Create a vector containing the words: apples, oranges, bananas
 
 ################################################################################
@@ -63,7 +67,6 @@ vec[rep(c(TRUE,FALSE),5)]
 
 my.list <- list(a = 1:10, b = 11:20, c = 'hello')
 my.list[['a']]
-##  [1]  1  2  3  4  5  6  7  8  9 10
 my.list$c
 
 # Most of the time you’ll be working with data frames (unless you’re working with ts), 
@@ -73,6 +76,9 @@ my.df <- data.frame(a = 1:10, b = 11:20, c = rep('hello',10))
 my.df$d <- sample(c(TRUE, FALSE), 10, replace = TRUE)
 head(my.df)
 
+# Challenge
+# Use the summary() function to generate summary statistics from your my.df data frame.
+
 ################################################################################
 ## FUNCTIONS
 ################################################################################  
@@ -81,10 +87,8 @@ head(my.df)
 # Keep in mind that many R functions need you to explicitly handle NAs
 
 mean(my.df$d)
-## [1] 0.5
 my.df$d[1] <- NA
 mean(my.df$d)
-## [1] NA
 mean(my.df$d, na.rm = TRUE)
 
 data("CO2")
@@ -121,15 +125,17 @@ data("CO2")
 
 # Example 1
 library(tidyverse)
-library(atus)
-data("atusact")
-
+load("data/atusact.rda")
+head(atusact)
+str(atusact)
+summary(atusact)
 # Make sure to overwrite old data so it updates!
-atusact <- atusact %>%
-  # Get two-digit and four-digit activity codes
+# Get two-digit and four-digit activity codes
+# Keep just personal care activities
+atusact <- atusact %>% 
+  ungroup() %>%
   mutate(two.digit.activity = floor(tiercode/10000),
-         four.digit.activity = floor(tiercode/100)) %>%
-  # Keep just personal care activities
+         four.digit.activity = floor(tiercode/100)) %>% 
   filter(two.digit.activity == 1)
 
 # Example 2
@@ -144,30 +150,39 @@ atusact_summary <- atusact %>%
   arrange(-mean.dur)
 atusact_summary  
 
+################################################################################
+# REGRESSIONS
+################################################################################
 
-# Regressions
-# Regression functions in R create regression objects
-# 
+# IMPORTANT: Regression functions in R create regression objects
+
 # Inputs: a formula object, a data set, options
 # Outputs: a regression object, generally intended to be stored and then run through some other function
 # To-do: look at the outcome! summary(regression.object) often a good option. Even better is stargazer(regression.object)
 # To-do: make use of the regression! Perhaps run it through predict(), or get relevant statistics from it with $
 #   Some of the relevant statistics are in the summary() not the regression object itself
 
-
+load("data/atusresp.rda")
 my.reg <- lm(hourly_wage ~ hh_size + work_hrs_week, data = atusresp)
 my.reg2 <- lm(hourly_wage ~ hh_size + work_hrs_week + ptft, data = atusresp)
 reg.predictions <- predict(my.reg)
+
+
+my.reg
+summary(my.reg)
 summary(my.reg)$r.squared
 
 # Significance stars are a common headache - note standards don’t match econ in default regression output
-
 summary(my.reg)
 
+################################################################################
 ## Regression Tables
-# stargazer is a good option, and I have used it in the past. Print table to the screen with type='text', or to a turn-in-able file with type = 'html', out = 'filename.html'. All of its defaults are what economists would expect
+################################################################################
 
-stargazer(my.reg, my.reg2, type = 'html', keep = c('hh_size', 'ptft'))
+# stargazer is a popular option. Print table to the screen with type='text', or to a turn-in-able file with type = 'html', out = 'filename.html'. All of its defaults are what economists would expect
+
+library(stargazer)
+stargazer(my.reg, my.reg2, type = 'text', keep = c('hh_size', 'ptft'))
 
 # another option is jtools
 # - handles weird regression types
@@ -176,40 +191,49 @@ stargazer(my.reg, my.reg2, type = 'html', keep = c('hh_size', 'ptft'))
 # - plot_coefs() for easy dot-and-CI plots of regression coefs.
 # - Note summ() won’t do sig stars. export_summs() will, although you must set them to econ levels by hand.
 
+library(jtools)
 export_summs(my.reg, my.reg2, robust = TRUE, stars = c(`***` = 0.01, `**` =
                                                          0.05, `*` = 0.1), coefs = 'hh_size')
 
 ## Regression Graphs in jtools
-data(mtcars); carsreg <- lm(mpg~hp+I(hp^2)+cyl, data = mtcars)
+data(mtcars)
+carsreg <- lm(mpg~hp+I(hp^2)+cyl, data = mtcars)
 effect_plot(carsreg, pred = hp, plot.points = TRUE, interval = TRUE)
 
 plot_coefs(my.reg, my.reg2)
 
-
+################################################################################
 # Regression Formulas
-# outcome ~ independent.variable + independent.variable
-# Factor variables will automatically be turned to dummies. Can do y ~ x + factor(z) to be sure
-# Interactions with *, y ~ x*z, or just-interaction-not-individual with :, y ~ x + x:z
-# Full interaction sets: y ~ (x1 + x2 + x3)^2 gives all two-way interactions, ^3 adds on the three-way
-# Functions can go straight in: ln(y) ~ x
-# Do calculations on variables first with I(): I(y == 1) ~ I(x^2)
-# Lots of variables? y~. regresses on everything in the data but y. In the tidyverse, combine this with “tidyselect” helpers like select(starts_with('gdp_'))
+################################################################################
+
+# - outcome ~ independent.variable + independent.variable
+# - Factor variables will automatically be turned to dummies. Can do y ~ x + factor(z) to be sure
+# - Interactions with *, y ~ x*z, or just-interaction-not-individual with :, y ~ x + x:z
+# - Full interaction sets: y ~ (x1 + x2 + x3)^2 gives all two-way interactions, ^3 adds on the three-way
+# - Functions can go straight in: ln(y) ~ x
+# - Do calculations on variables first with I(): I(y == 1) ~ I(x^2)
+# - Lots of variables? y~. regresses on everything in the data but y. In the tidyverse, 
+#   combine this with “tidyselect” helpers like select(starts_with('gdp_'))
 
 
-# Regression Commands
-# lm() is standard OLS
-# Time series: see the packages dynlm, forecast, tseries
-# Pretty much all micro (robust SEs, clustering, IV, fixed effects) can be done with the estimatr (lm_robust(), iv_robust()) or lfe (felm()) packages. Former has easier syntax but is less powerful and doesn’t work with stargazer() - use jtools (export_summs()) or huxtable (huxreg()) instead.
-# Joint F-tests with linearHypothesis() in car
-# For anything: google “R my-thing” or “rstats my-thing”
+## Regression Commands
 
+# - lm() is standard OLS
+# - Time series: see the packages dynlm, forecast, tseries
+# - Pretty much all micro (robust SEs, clustering, IV, fixed effects) can be done with the estimatr (lm_robust(), iv_robust()) or lfe (felm()) packages. Former has easier syntax but is less powerful and doesn’t work with stargazer() - use jtools (export_summs()) or huxtable (huxreg()) instead.
+# - Joint F-tests with linearHypothesis() in car
+# - For anything: google “R my-thing” or “rstats my-thing”
 
-# Graphing
+################################################################################
+# GRAPHING
+################################################################################
+
 # ggplot2 (ggplot()) is very much a top-tier graphing tool
 # Each graph consists of: data, an aes()thetic (~axes), and a geometry (what you draw)
 # Not actually too difficult to use; syntax for advanced styling can be difficult, but shouldn’t be too necessary for UGs. Stuff like legends and text styling can be made easier with the ggeasy package.
 
-mtcars <- mtcars %>% mutate(Transmission = factor(am, labels = c("Automatic", "Manual")))
+mtcars <- mtcars %>% 
+  mutate(Transmission = factor(am, labels = c("Automatic", "Manual")))
 ggplot(mtcars, aes(x = hp, y = mpg, shape = Transmission, color = Transmission)) + geom_point() + 
   theme_minimal() +
   geom_smooth(method = 'lm') + 
